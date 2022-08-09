114854be2f1afe9d78cac85f33b446354b775d26
fishyeko <106758525+fishyeko@users.noreply.github.com>
2022-08-09 10:39:40 +0300

    Update print_char.c

--- print_char.c
+++ print_char.c
@@ -1,16 +1,28 @@
-#include "main.h"
-/**
- * print_char - print characters
- * @a: element of va_list type
- * @s: buffer
- * @index: position on buffer
- * Return: number of "characters" of element called
- */
-int print_char(va_list a, char *s, int *index)
-{
-	int x;
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "holberton.h"
 
-	x = va_arg(a, int);
-	set_buffer(s, x, index);
-	return (1);
+/**
+ * print_char - prints character
+ * @c: char to be prited
+ * Return 1
+ */
+
+int print_char(va_list c)
+{
+unsigned char my_ch;
+my_ch = va_arg(c, int);
+_putchar(my_ch);
+return (1);
+}
+
+/**
+ * print_percent - prints persentage
+ * Return: 1
+ */
+int print_percent(void)
+{
+_putchar('%');
+return (1);
 }
