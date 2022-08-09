8638c5cedfe32785c0200b1464421ae032f75b38
fishyeko <106758525+fishyeko@users.noreply.github.com>
2022-08-09 11:35:06 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,41 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <limits.h>
+#include <stdarg.h>
+#include <stdio.h>
 /**
- * struct a - this structure is going to give us the format
- * @c: the character that is going to give us the respective function
- * @p: function pointer with va_list argument
+ *struct print_a - A struct that has a pointer character and a pointer
+ *to a function
+ *@s: a character pointer
+ *@f: a function pointer
  */
-typedef struct a
+typedef struct print_a
 {
-	    char c;
-	    int (*p)(va_list list, char *s, int *index);
-} choose;
+	char s;
+	int (*f)();
+} print_a_t;
+int check_formatter(va_list args, const char *format, print_a_t print_a[]);
 int _printf(const char *format, ...);
 int _putchar(char c);
-void set_buffer(char *s, char x, int *index);
-int (*get_function(char c))(va_list a, char *s, int *index);
-int print_char(va_list a, char *s, int *index);
-int print_string(va_list a, char *s, int *index);
-int print_percent(va_list a, char *s, int *index);
-int print_integer(va_list a, char *s, int *index);
-int print_binary(va_list a, char *s, int *index);
-int print_unsigned(va_list a, char *s, int *index);
-int convert_octal(int *a, int b, char *s, int *index);
-int print_octal(va_list a, char *s, int *index);
-int get_HEX(int a, char *s, int *index);
-int convert_hex(int *a, int b, char *s, int *index);
-int convert_HEX(int *a, int b, char *s, int *index);
-int print_hex(va_list a, char *s, int *index);
-int print_HEX(va_list a, char *s, int *index);
-int print_special_char(va_list a, char *s, int *index);
-int switch_hex(int sum, int *h, int y);
-int switch_HEX(int sum, int *h, int y);
-int print_Rot13(va_list a, char *s, int *index);
-int print_reverse(va_list a, char *s, int *index);
-int print_address(va_list a, char *s, int *index);
-
+int print_number(int n);
+void a_struct(void);
+int print_per(int p);
+int print_rev(va_list args);
 #endif
